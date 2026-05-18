abee6b21a5ff6e631d3d970c56d1be4788155754
Hendrik <hendrik_boettcher@yahoo.de>
2026-05-19 01:13:54 +0200

    validate input name
    - refactor modal to class
    - refactor page scripts for people and index
    - reject empty new person name with error message

--- src/app/static/js/modals/modal.js
+++ src/app/static/js/modals/modal.js
@@ -1,7 +1,34 @@
-function openModal(modalId) {
-  document.getElementById(modalId).classList.remove("hidden");
-}
+export class Modal {
+  constructor(element) {
+    this.element = element;
+    this.closeButton = this.element.querySelector(".modal-close");
+    this.cancelButton = this.element.querySelector(".cancel-btn");
+    this.handleKeydown = this.handleKeydown.bind(this);
+    document.addEventListener("keydown", this.handleKeydown);
+    this.setupEvents();
+  }
 
-function closeModal(modalId) {
-  document.getElementById(modalId).classList.add("hidden");
+  open() {
+    this.element.classList.remove("hidden");
+  }
+
+  close() {
+    this.element.classList.add("hidden");
+  }
+
+  setupEvents() {
+    this.closeButton?.addEventListener("click", () => this.close());
+
+    this.cancelButton?.addEventListener("click", () => this.close());
+  }
+
+  handleKeydown(e) {
+    if (e.key === "Escape") {
+      this.close();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener("keydown", this.handleKeydown);
+  }
 }
---
+++ src/app/static/js/modals/new_person.js
@@ -0,0 +1,65 @@
+import { Modal } from "./modal.js";
+
+export class NewPersonModal {
+  constructor(socket) {
+    this.root = document.getElementById("person-modal");
+    this.nameInput = document.getElementById("person-name");
+    this.saveButton = this.root.querySelector(".save-btn");
+    this.socket = socket;
+    this.modal = new Modal(this.root);
+    this.setupEvents();
+  }
+
+  open() {
+    this.modal.open();
+    this.#resetInputFields();
+  }
+
+  close() {
+    this.modal.close();
+  }
+
+  setupEvents() {
+    this.saveButton?.addEventListener("click", () => this.save());
+  }
+
+  #resetInputFields() {
+    this.nameInput.value = "";
+    this.#setFieldError(this.nameInput, "");
+  }
+
+  #setFieldError(inputEl, message) {
+    const inputId = inputEl.id;
+    const errorEl = document.getElementById(`${inputId}-error`);
+    if (message) {
+      inputEl.classList.add("error");
+      inputEl.parentElement.classList.add("error");
+      errorEl.textContent = message;
+    } else {
+      inputEl.classList.remove("error");
+      inputEl.parentElement.classList.remove("error");
+      errorEl.textContent = "";
+    }
+  }
+
+  #isValidInput() {
+    let isValid = true;
+    if (!this.nameInput.value) {
+      isValid = false;
+      this.#setFieldError(this.nameInput, "Der Name fehlt.");
+    } else {
+      this.#setFieldError(this.nameInput, "");
+    }
+    return isValid;
+  }
+
+  save() {
+    if (!this.#isValidInput()) {
+      return;
+    }
+    this.socket.emit("request_create_person", {
+      name: this.nameInput.value,
+    });
+    this.close();
+  }
+}
